ccba1b8419ea336c3eae2a8c494f418973f2a01c
GasaGentille <gisagentille55@gmail.com>
2019-10-25 14:46:03 +0200

    Create Project and Profile function

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    project = models.ImageField(upload_to = 'projects/',null=True )
+    project_name = models.CharField(max_length =30)
+    project_caption = models.CharField(max_length =30)
+    user = models.ForeignKey(User,on_delete=models.CASCADE ,null=True )
+    
+    
+    
+
+    def __str__(self):
+        return self.project_name
+
+    def save_project(self):
+        self.save()
+
+    def delete_delete(self):
+        self.delete()
+
+
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to = 'images/', null=True)
+    bio = models.CharField(max_length =30)
+    user = models.OneToOneField(User,on_delete=models.CASCADE ,related_name="profile",null=True)
+    project = models.ForeignKey(Project,null=True)
+
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_profile(cls, username):
+        return cls.objects.filter(name__icontains=username)
+
+    @classmethod
+    def search_by_user(cls,search_term):
+        profile = cls.objects.filter(title__icontains=search_term)
+        return profile
+
+    def save_profile(self):
+        self.user
+    def delete_profile(self):
+        self.delete()
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from .models import Project,Profile
+from django.contrib.auth.decorators import login_required
+from .forms import NewProjectForm,ProfileForm
+
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def project(request):
+    projects = Project.objects.all()
+    return  render (request,'index.html',{"projects":projects})
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username=None):
+    current_user = request.user
+    prjects = Image.objects.filter(user = current_user)
+    return  render (request,'profile.html',locals(),{"prjects":prjects})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('/')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    prof_update = Profile.objects.filter(user=current_user).first()
+    images = Image.objects.filter(user=current_user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+    
+        if form.is_valid():
+            project=form.save(commit=False)
+            project.user=current_user
+            project.save()
+            
+            return redirect('profile')
+        
+    else:
+        
+        form = ProfileForm()
+    return render(request, 'add_profile.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user
+    prof_update = Profile.objects.filter(user=current_user).first()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+    
+        if form.is_valid():
+            profile=form.save(commit=False)
+            Profile.objects.filter(id = prof_update.id).update(profile_photo =profile.profile_photo,bio=profile.bio)
+            
+            return redirect('profile')
+        
+    else:
+        
+        form = ProfileForm()
+    return render(request, 'update.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_user = Profile.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"user": searched_user})
+
+    else:
+        message = "You haven't searched for any user"
+        return render(request, 'search.html',{"message":message})
+
+
