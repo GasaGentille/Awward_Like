34186672cda7b1415a3f7dc7649f5c8320c5b9f7
GasaGentille <gisagentille55@gmail.com>
2019-10-31 15:20:20 +0200

    Testing profile

--- posts/tests.py
+++ posts/tests.py
@@ -5,7 +5,7 @@ from .models import Project,Profile,Review
 class ProjectTestClass(TestCase):
 
     def setUp(self):
-        # Creating a new image and saving it
+        # Creating a new project and saving it
         self.new_project= Project(project ='projects/', title ='galery', project_description = "Django galery")
 
     # Testing  instance
@@ -26,3 +26,28 @@ class ProjectTestClass(TestCase):
         delete = Project.objects.filter(title = project.title).delete()
         projects = Project.objects.all()
         self.assertFalse(len(projects)==1)
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new profile and saving it
+        self.new_profile= Profile(profile_photo ='images/', bio ='pray')
+
+    # Testing  instance
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+   # Testing Save Method
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertFalse(len(profiles) > 0) 
+
+    #test delete
+    def test_delete_profile(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.filter(bio='pray').first()
+        delete = Profile.objects.filter().delete()
+        profiles = Profile.objects.all()
+        self.assertFalse(len(profiles)==1)
